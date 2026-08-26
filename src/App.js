9395399ac2958e65c724c5c68a2bbf009153f56f
MS <sunil.mule@ibm.com>
2026-08-26 15:35:01 +0530

    Added Route for pages in view folder

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
+
+import Home from './Views/Home';
+import About from './Views/About';
+import Contact from './Views/Contact';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  <BrowserRouter>
+      <nav>
+        <Link to="/">Home</Link> |{" "}
+        <Link to="/about">About</Link> |{" "}
+        <Link to="/con">Contact</Link>
+      </nav>
+
+      {/* Routes */}
+      <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/con" element={<Contact />} /> 
+      </Routes>
+  </BrowserRouter>
   );
 }
 
 export default App;
+
+
